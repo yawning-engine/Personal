49ab184e48fd62394e9fa69c9bf546e91d6a43f0
yawning-engine <bhuvandb36@gmail.com>
2021-10-03 21:55:56 +0530

    fixed secondary arr num value storage

--- cpp/pointertopointers.cpp
+++ cpp/pointertopointers.cpp
@@ -14,6 +14,21 @@ int main()
 	for(i=0;i<n;i++)
 	{
 		cin>>m;	
-		*nums[i] = new int[m]
+		nums[i] = new int[m];
+		nums[i][0] = m;
+		for (j=1;j<=m;j++)
+		{
+			cin>>nums[i][j];
+		}
 	}
+	for(i=0;i<n;i++)
+	{
+		for (j=1;j<=nums[i][0];j++)
+		{
+			cout<<nums[i][j]<<" ";
+		}
+		cout<<"\n";
+	}
+	
+
 }
