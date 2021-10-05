35ac05918d579d0d542ef48c7d19ebdcb3679a91
yawning-engine <bhuvandb36@gmail.com>
2021-10-05 15:10:34 +0530

    segmentation fault

--- cpp/stringstream_parseint.cpp
+++ cpp/stringstream_parseint.cpp
@@ -3,17 +3,33 @@
 #include <iostream>
 using namespace std;
 
-vector<int> parseInts(string str) {
-	// Complete this function
+vector<int> parseInts(string str) 
+{
+	stringstream ss;
+	vector<int> nums;
+	int i,num;
+	string temp;
+	ss << str;
+	while(getline(ss,temp,','))
+	{
+		//num = int(temp);
+		//nums.push_back(num);
+		cout<<temp;
+	}
+	for(i=0;i<nums.size();i++)
+		cout<<nums[i];
+	
 }
 
-int main() {
-    string str;
-    cin >> str;
-    vector<int> integers = parseInts(str);
-    for(int i = 0; i < integers.size(); i++) {
-        cout << integers[i] << "\n";
-    }
+int main() 
+{
+	string str;
+	cin >> str;
+	vector<int> integers = parseInts(str);
+	for(int i = 0; i < integers.size(); i++) 
+	{
+		cout << integers[i] << "\n";
+	}
     
-    return 0;
+	return 0;
 }
