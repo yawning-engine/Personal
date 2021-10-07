062ecf122a909f344f692ae4768f3e23bc7471a1
yawning-engine <bhuvandb36@gmail.com>
2021-10-07 17:05:44 +0530

    query parse

--- cpp/attribute_parse.cpp
+++ cpp/attribute_parse.cpp
@@ -7,7 +7,28 @@ using namespace std;
 
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    string tags[20], query[20],line,value,tagnum;
+    int n,q,i,j,tpos,vpos;
+    cin>>n>>q;
+    for(i=0;i<n;i++)
+    	cin>>tags[i];
+    for(i=0;i<q;i++)
+    	cin>>query[i];
+    for(i=0,line=query[i];i<q;i++)
+    {
+    	vpos = line.find('~');
+    	value = line.substr(vpos);
+    	tpos = 0;
+    	for(j=0;line[j] != '~';j++)
+    	{
+		if(line[j] == '.')
+			tpos = j+1;
+    	
+    	}
+	//tag = line.substr(tpos,vpos);
+	tagnum = line.substr(tpos+3,vpos);
+    }
+    
     return 0;
 }
 
