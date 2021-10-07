2186f6af07cdfbaeb6bc691549d088ab5503b1c9
yawning-engine <bhuvandb36@gmail.com>
2021-10-07 21:26:02 +0530

    find() out of bounds fixed

--- cpp/attribute_parse.cpp
+++ cpp/attribute_parse.cpp
@@ -19,8 +19,9 @@ int main() {
     {
     	cin>>query[i];
     }
-    for(i=0,line=query[i];i<q;i++)
+    for(i=0;i<q;i++)
     {
+    	line=query[i];
     	vpos = line.find('~');
     	value = line.substr(vpos+1); //value of the attribute to be found
     	tpos = 0;
@@ -33,12 +34,15 @@ int main() {
 	//tag = line.substr(tpos,vpos);
 	tagnum = stoi(line.substr(tpos+3,vpos)); //tag number
 	
-	line = tags[tagnum-1]; 
-	if(line.find(value))
+	line = tags[tagnum-1];
+	cout<<"query:"<<i<<" value:"<<value<<endl; 
+	if((line.find(value)>=0) && (line.find(value)<=line.length()))
 	{
 		findval = line.substr(line.find(value)+value.length()+4);
 		quote = findval.find('\"'); // position of closing quote of the attribute
+		cout<<"query:"<<i<<" value index:"<<line.find(value)<<endl;
 		cout<<findval.substr(0,quote)<<endl;
+		
 	}
 	else
 		cout<<"Not Found!"<<endl;
