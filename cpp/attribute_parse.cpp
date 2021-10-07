65963f5bde25f1454d4ee213eb27a9f50f5c3a2c
yawning-engine <bhuvandb36@gmail.com>
2021-10-07 17:37:05 +0530

    parse tags, out of range error

--- cpp/attribute_parse.cpp
+++ cpp/attribute_parse.cpp
@@ -7,8 +7,8 @@ using namespace std;
 
 
 int main() {
-    string tags[20], query[20],line,value,tagnum;
-    int n,q,i,j,tpos,vpos;
+    string tags[20], query[20],line,value,findval;
+    int n,q,i,j,tpos,vpos,quote,tagnum;
     cin>>n>>q;
     for(i=0;i<n;i++)
     	cin>>tags[i];
@@ -26,8 +26,19 @@ int main() {
     	
     	}
 	//tag = line.substr(tpos,vpos);
-	tagnum = line.substr(tpos+3,vpos);
+	tagnum = stoi(line.substr(tpos+3,vpos));
+	
+	line = tags[tagnum-1];
+	if(line.find(value))
+	{
+		findval = line.substr(line.find(value)+value.length()+4);
+		quote = findval.find('\"');
+		cout<<findval.substr(0,quote)<<endl;
+	}
+	else
+		cout<<"Not Found!"<<endl;
     }
+    
     
     return 0;
 }
