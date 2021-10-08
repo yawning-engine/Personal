b2160f3a99e9fb03967a11fa45043b38f79df0e6
yawning-engine <bhuvandb36@gmail.com>
2021-10-08 14:05:36 +0530

    complete logic rework

--- cpp/attribute_parse.cpp
+++ cpp/attribute_parse.cpp
@@ -8,8 +8,8 @@ using namespace std;
 
 
 int main() {
-    string tags[20], query[20],searchorder[20],line,value,findval,endtag,tag;
-    int n,q,i,j,k,tpos,vpos,dotcount,found,size;
+    string tags[20], query[20],searchorder[20],line,value,tag,tagstack[20];
+    int n,q,i,j,k,ts,tpos,dotcount;
     cin>>n>>q;
     for(i=0;i<n;i++)
     {
@@ -19,13 +19,16 @@ int main() {
     {
     	cin>>query[i];
     }
+/*        for(i=0;i<tagnum;i++)
+    {
+    	cout<<tagnames[i]<<endl;
+    } */
     for(i=0;i<q;i++)
     {
     	line=query[i];
     	value = line.substr(line.find('~')+1); //value of the attribute to be found
     	tpos = 0;
     	dotcount=0;
-    	size=0;
     	for(j=0;line[j] != '~';j++) //pos of supertag in case of nested tags
     	{
 		if(line[j] == '.')
@@ -34,48 +37,50 @@ int main() {
 			tpos = j+1;
 			dotcount++;
 		}
-		
     	
     	}
     	searchorder[dotcount]=line.substr(tpos,line.find('~')-tpos);
     	tag = "<" + searchorder[dotcount];
-    	for(j=0;j<=dotcount;j++)
+    	for(j=n-1,k=0;j>=0;j++)
     	{
-    		endtag = "</" + searchorder[j];
-    		found=0;
-    		for(k=n-1;k>=0;k--)
-    		{
-    			if(tags[k].find(endtag) != -1) 
-    			{
-    				found = 1;
-    				size++;
-    			}
-    			if ((size <= dotcount) && (found == 1))
-    				break;
-    			if ((size > dotcount) && (found == 0))
-    			{
-    				cout<<"Not Found!"<<endl;
-    				break;
-    			}
-    			if ((size > dotcount) && (found == 1))
-    			{
-    				if(tags[k].find(tag) != -1)
-    				{
-    					vpos = tags[k].find(value);
-    					if((vpos >= 0) && (vpos < tags[k].length()))
-    					{
-    						findval =tags[k].substr(vpos + value.length() + 4);
-    						cout<<findval.substr(0,findval.find("\""))<<endl;
-    						break;
-    					}
-    					else
-    					{
-    						cout<<"Not Found!"<<endl;
-    						break;
-    					}
-    				}
-    			}	
-    		}
+		if(tags[j].find(tag) != -1)
+		{
+			for(ts=0;ts<k;ts++)
+			{
+				if(tagstack[ts] == searchorder[ts])
+					{
+						if (tagstack[ts] == searchorder[dotcount])
+						{
+							if(tags[j].find(value) != -1)
+							{
+								cout<<tags[j].substr(tags[j].find(value),tags[j].find("/")-tags[j].find(value))<<endl;
+								break;
+							}
+							else
+							{
+								cout<<"Not Found!"<<endl;
+								break;
+								
+							}						
+						}
+					}
+				else
+				{
+					cout<<"Not Found!"<<endl;
+					break;
+				}
+			}
+		}
+		if(tags[j].find("/") != -1)
+		{
+			tagstack[k]=tags[j].substr(tags[j].find("/"),tags[j].find(" ")-tags[j].find("/"));
+			k++;
+		}
+		else
+		{
+			k--;  
+		}
+		 	
     	}
 	
     }
